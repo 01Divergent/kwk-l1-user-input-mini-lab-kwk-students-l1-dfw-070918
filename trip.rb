723c5c3d04e61dd09afc460889ce18f80dc301d3
01Divergent <iphone.claudia@icloud.com>
2018-07-10 14:08:12 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -7,8 +7,8 @@ puts "Where would you like to eat?"
 
 eat = gets.chomp
 
-puts "How do you plan to spend the night?"
+puts "How many nights do you plan to stay?"
 
 nights = gets.chomp.capitalize
 
-puts "Great! So, you would you like to stay in #{stay} and eat at #{eat} near the #{nights}."
+puts "Great! So, you would you like to stay in #{stay} and eat at #{eat}, and spend #{nights} nights."
